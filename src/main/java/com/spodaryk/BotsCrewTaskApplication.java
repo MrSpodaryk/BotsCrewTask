2e61ad9a0dbf04fb9c43cdf80ebd80cbf89b9ca6
Roman Spodaryk <roman.spodaryk@ukr.net>
2020-11-25 12:37:18 +0200

    created base for project

---
+++ src/main/java/com/spodaryk/BotsCrewTaskApplication.java
@@ -0,0 +1,12 @@
+package com.spodaryk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsCrewTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsCrewTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spodaryk/BotsCrewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spodaryk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsCrewTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
